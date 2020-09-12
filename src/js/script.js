1f2c93fcc7deed54ab165b6f64e58b0c23610e49
Demeqrosad <demetriusz@kurosad.pl>
2020-09-12 22:25:06 +0200

    Add accordion functionality on products menu

--- src/js/script.js
+++ src/js/script.js
@@ -88,26 +88,30 @@
     {
       const thisProduct = this;
       /* find the clickable trigger (the element that should react to clicking) */
-
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
       /* START: click event listener to trigger */
-
+      clickableTrigger.addEventListener('click', function()
+      {
         /* prevent default action for event */
-
+        event.preventDefault();
         /* toggle active class on element of thisProduct */
-
+        thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
         /* find all active products */
-
+        const activeProducts = document.querySelectorAll(select.all.menuProductsActive);
         /* START LOOP: for each active product */
-
+        for(let activeProduct of activeProducts)
+        {
           /* START: if the active product isn't the element of thisProduct */
-
+          if(activeProduct != thisProduct.element)
+          {
             /* remove class active for the active product */
-
+            activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
           /* END: if the active product isn't the element of thisProduct */
-
+          }
         /* END LOOP: for each active product */
-
+        }
       /* END: click event listener to trigger */
+      });
     }
   }
 
