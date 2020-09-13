789a8818415f57e2c66b79a1f7c895683e651573
Demeqrosad <demetriusz@kurosad.pl>
2020-09-13 18:03:46 +0200

    Add event listeners to options

--- src/js/script.js
+++ src/js/script.js
@@ -62,7 +62,11 @@
       thisProduct.data = data;
 
       thisProduct.renderInMenu();
+      thisProduct.getElements();
       thisProduct.initAccordion();
+      thisProduct.initOrderForm();
+      thisProduct.processOrder();
+
 
       console.log('new Product: ', thisProduct);
     }
@@ -84,13 +88,24 @@
       menuContainer.appendChild(thisProduct.element);
     }
 
+    getElements()
+    {
+      const thisProduct = this;
+
+      thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
+      thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
+      thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
+      thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+    }
+
     initAccordion()
     {
       const thisProduct = this;
       /* find the clickable trigger (the element that should react to clicking) */
-      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      //const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
       /* START: click event listener to trigger */
-      clickableTrigger.addEventListener('click', function()
+      thisProduct.accordionTrigger.addEventListener('click', function()
       {
         /* prevent default action for event */
         event.preventDefault();
@@ -113,6 +128,40 @@
       /* END: click event listener to trigger */
       });
     }
+
+    initOrderForm()
+    {
+      const thisProduct = this;
+
+      thisProduct.form.addEventListener('submit', function(event)
+      {
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+
+      for(let input of thisProduct.formInputs)
+      {
+        input.addEventListener('change', function()
+        {
+          thisProduct.processOrder();
+        });
+      }
+
+      thisProduct.cartButton.addEventListener('click', function(event)
+      {
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+    }
+
+    processOrder()
+    {
+      const thisProduct = this;
+
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      console.log('formData', formData);
+    }
+
   }
 
   const app = {
