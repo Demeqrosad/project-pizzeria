d4105dbb39204d23d4b348a09d7b08ee832aeed8
Demeqrosad <demetriusz@kurosad.pl>
2020-09-18 20:28:26 +0200

    Add toggle cart rolling down

--- src/js/script.js
+++ src/js/script.js
@@ -350,6 +350,43 @@
     }
   }
 
+  class Cart
+  {
+    constructor(element)
+    {
+      const thisCart = this;
+
+      thisCart.products = [];
+
+      thisCart.getElements(element);
+
+      thisCart.initActions();
+
+      console.log('new Cart', thisCart);
+    }
+
+    getElements(element)
+    {
+      const thisCart = this;
+
+      thisCart.dom = {};
+
+      thisCart.dom.wrapper = element;
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+    }
+
+    initActions()
+    {
+      const thisCart = this;
+
+      thisCart.dom.toggleTrigger.addEventListener('click', function(event)
+      {
+        event.preventDefault();
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      });
+    }
+  }
+
   const app = {
     initMenu: function()
     {
@@ -370,7 +407,16 @@
       thisApp.data = dataSource;
     },
 
-    init: function(){
+    initCart: function()
+    {
+      const thisApp = this;
+
+      const cartElem = document.querySelector(select.containerOf.cart);
+      thisApp.cart = new Cart(cartElem);
+    },
+
+    init: function()
+    {
       const thisApp = this;
       console.log('*** App starting ***');
       console.log('thisApp:', thisApp);
@@ -380,6 +426,7 @@
 
       thisApp.initData();
       thisApp.initMenu();
+      thisApp.initCart();
     },
   };
 
